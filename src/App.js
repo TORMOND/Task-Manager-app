66d38421678d3453d6421aab72fbf99804735d33
Tormond <victormonderu@gmail.com>
2022-07-26 12:47:17 +0300

    made changes to the App and Home files

--- src/App.js
+++ src/App.js
@@ -1,55 +1,27 @@
 import Home from './Home';
 import './App.css';
-import SideBar from './SideBar';
-import Portfolio from './portfolio';
-import Create from './create';
+
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Project from './project';
 import Schedule from './SchedulePage';
-import { useState, useRef } from 'react';
 
 
 function App() {
 
-const [project,setProjects] = useState(null);
-const appRef = useRef()
-const createProject = ()=>{
-
-console.log("project initiated");
-setProjects(true)
-appRef.current.className = "active"
-}
-const closeSection = ()=>{
-  setProjects(false)
-  appRef.current.className = ''
-}
-
   return (
     <div className="App">
-      
-      <div className="create-projects">
-      {project && <Create closeSection={closeSection} />}
-  
-      </div>
-    
-<div ref={appRef}  id="app-container">
+
   <Router>
-  <SideBar createProject={createProject}></SideBar>
+  
   <Routes>
     <Route path='/' element={<Home />}></Route>
-
     <Route path='/Schedule' element={<Schedule />}></Route>
     <Route path='/project/:id' element={<Project />}></Route>
 
   </Routes>
 
-
 </Router>
 
-     
-  <Portfolio></Portfolio>  
-</div>
- 
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,44 @@
 import './index.css';
 import NavBar from './NavBar';
 import Todos from './Todos';
+import SideBar from './SideBar';
+// import Portfolio from './portfolio';
+import { useState, useRef } from 'react';
+import Create from './create';
 
 const Home = () => {
+    const [project,setProjects] = useState(null);
+    const appRef = useRef()
+    const createProject = ()=>{
+    
+    console.log("project initiated");
+    setProjects(true)
+    appRef.current.className = "active"
+    }
+    const closeSection = ()=>{
+      setProjects(false)
+      appRef.current.className = ''
+    }
     return ( 
         <template>
 
-            <div className="Home">
-                <NavBar HomePage="homepage" />
-                <Todos /> 
+            <div className="Home" >
+            <div className="create-projects">
+               {project && <Create closeSection={closeSection} />}
+               </div>
+                  
+                <div ref={appRef}  id="app-container">
+<SideBar createProject={createProject}></SideBar>  
+            <div className="section-two-main">      
+             <div>
+               <NavBar HomePage="homepage" />
+               <Todos />  
+             </div>
+                   
+            </div>
+        
+                </div>
+               
             </div>
             
         </template>
